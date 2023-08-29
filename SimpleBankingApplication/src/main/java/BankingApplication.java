eb2fc5c93f3bf8c6653c5d5997290cc8d679247b
git-aditi <aditithakkar1998@gmail.com>
2023-08-28 23:10:42 -0400

    First Commit

---
+++ SimpleBankingApplication/src/main/java/BankingApplication.java
@@ -0,0 +1,12 @@
+
+public class BankingApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		BankAccount obj1 = new BankAccount("XYZ", "BA0001");
+		obj1.showMenu();
+
+	}
+
+}
